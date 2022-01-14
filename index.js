2d434b66398d048e667d83ea940792afefb7adbb
Sam Estep <sam@samestep.com>
2022-01-14 17:00:49 -0500

    Add assignment wrapping example

---
+++ index.js
@@ -0,0 +1,4 @@
+// https://prettier.io/blog/2021/05/09/2.3.0.html
+// Prettier 2.3
+aParticularlyLongAndObnoxiousNameForIllustrativePurposes =
+  "a very long string for illustrative purposes".length;
---
+++ packages/foo/index.js
@@ -0,0 +1,4 @@
+// https://prettier.io/blog/2021/05/09/2.3.0.html
+// Prettier 2.2
+aParticularlyLongAndObnoxiousNameForIllustrativePurposes = "a very long string for illustrative purposes"
+  .length;
